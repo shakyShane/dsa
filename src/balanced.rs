2983abd713e224a53a2a0c36ded081cf0ea16d51
Shane Osbourne <shane.osbourne8@gmail.com>
2021-04-23 22:32:06 +0100

    support more chars

--- src/balanced.rs
+++ src/balanced.rs
@@ -1,12 +1,21 @@
 // O(n)
 fn balanced(input: &str) -> bool {
-    let mut stack: Vec<char> = vec![];
+    let mut stack: Vec<Char> = vec![];
+    enum Char {
+        Paren,
+        Square,
+        Brace,
+    }
     for c in input.chars() {
         match c {
-            '(' => stack.push('('),
-            ')' => match stack.pop() {
-                Some('(') => {}
-                _ => return false,
+            '(' => stack.push(Char::Paren),
+            '[' => stack.push(Char::Square),
+            '{' => stack.push(Char::Brace),
+            ')' | ']' | '}' => match (c, stack.pop()) {
+                (')', Some(Char::Paren)) => {}
+                (']', Some(Char::Square)) => {}
+                ('}', Some(Char::Brace)) => {}
+                (_, _) => return false,
             },
             _ => {}
         }
@@ -16,9 +25,15 @@ fn balanced(input: &str) -> bool {
 
 #[test]
 fn test_balanced() {
+    assert_eq!(balanced("[]"), true);
+    assert_eq!(balanced("["), false);
     assert_eq!(balanced("(())"), true);
     assert_eq!(balanced("((()"), false);
     assert_eq!(balanced(")(())"), false);
     assert_eq!(balanced("))))"), false);
     assert_eq!(balanced("(()))("), false);
+    assert_eq!(balanced("([])"), true);
+    assert_eq!(balanced("([[[]]])"), true);
+    assert_eq!(balanced("([[[00]])"), false);
+    assert_eq!(balanced("([[[{0}]]])"), true);
 }
